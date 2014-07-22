024033f3f3cc433c012187b7eb8ff8ecd9cd1523
MaazAli <maaz_ali@outlook.com>
2014-07-21 20:43:28 -0700

    Added in casting model for the front end

---
+++ front/app/models/casting.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+var attr = DS.attr,
+	belongsTo = DS.belongsTo,
+	hasMany = DS.hasMany;
+
+export default DS.Model.extend({
+	language: attr(),
+	role: attr(),
+	person_id: belongsTo('person'),
+	character_id: belongsTo('character'),
+	anime_id: belongsTo('anime')
+});
