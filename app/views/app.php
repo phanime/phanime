681d269f00e3dab2500615e3711372eaf2502f35
MaazAli <maaz_ali@outlook.com>
2014-07-25 16:22:36 -0700

    Refactoring code splitting large templates into smaller partials

--- app/views/app.php
+++ app/views/app.php
@@ -9,16 +9,16 @@
 
     <base href="/" />
 
-    <link rel="stylesheet" href="assets/vendor.css">
-    <link rel="stylesheet" href="assets/front.css">
+    <link rel="stylesheet" href="assets/vendor-75f589fb49f0c57c79e647241c1b4e49.css">
+    <link rel="stylesheet" href="assets/front-2d7cc4d9a21d3d6d1b8c839c7a37b59d.css">
   </head>
   <body>
     <script>
-      window.FrontENV = {"environment":"development","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_MODULE_RESOLVER":true,"LOG_VIEW_LOOKUPS":true},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"},"LOG_MODULE_RESOLVER":true};
+      window.FrontENV = {"environment":"production","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"}};
       window.EmberENV = window.FrontENV.EmberENV;
     </script>
-    <script src="assets/vendor.js"></script>
-    <script src="assets/front.js"></script>
+    <script src="assets/vendor-016b1f31fb6ccd14c92407719587d2d1.js"></script>
+    <script src="assets/front-bdc36302a0a327a0c376aff08072184a.js"></script>
     <script>
       window.Front = require('front/app')['default'].create(FrontENV.APP);
     </script>
