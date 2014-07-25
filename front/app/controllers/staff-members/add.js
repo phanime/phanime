662755090bccaad30e23a2a233917de4361403a3
MaazAli <maaz_ali@outlook.com>
2014-07-24 20:32:21 -0700

    Template finished for staff member creations, controller is still under progress

--- front/app/controllers/staff-members/add.js
+++ front/app/controllers/staff-members/add.js
@@ -2,48 +2,66 @@ import Ember from 'ember';
 import Notify from 'ember-notify';
 
 export default Ember.ObjectController.extend({
-	cover_photo: null,
-	first_name: null,
-	last_name: null,
-	japanese_name: null,
-	alternate_name: null,
-	gender: null,
-	biography: null,
+	staff_position: null,
 	actions: {
-		add_character: function() {
+		add_staffMember: function() {
 
 			var store = this.store;
 			//var self = this;
 
-			var character = store.createRecord('character', {
-				cover_photo: '',
-				first_name: this.get('first_name'),
-				last_name: this.get('last_name'),
-				japanese_name: this.get('japanese_name'),
-				alternate_name: this.get('alternate_name'),
-				gender: this.get('gender'),
-				biography: this.get('biography'),
+			var staffMember = store.createRecord('staffMember', {
+				person_id: this.get('selectedPerson'),
+				anime_id: this.get('selectedAnime'),
+				staff_position: this.get('staff_position'),
 			});
 
 
-			var onSuccess = function(character) {
-
-				var msg = character.get('first_name') + " " + character.get('last_name') + " was successfully added.";
+			var onSuccess = function(staffMember) {
+				var msg = "Staff member with staff position: " + staffMember.get('staff_position') + " was successfully added.";
 				console.log(msg);
 				Notify.success(msg);
-				//self.transitionTo('character', character);
 			};
 
-			var onFailure = function(character) {
-				var msg = "Something went wrong, " + character.get('first_name') + " " + character.get('last_name') + " was not added.";
+			var onFailure = function() {
+				var msg = "Something went wrong, staff member was not added.";
 				console.log(msg);
 				Notify.warning(msg);
 			};
 
-			character.save().then(onSuccess, onFailure);
+			staffMember.save().then(onSuccess, onFailure);
 
-		}
+		},
+		trigger_search_anime: function() {
+			var store = this.store;
+
+			var search_results = store.filter('anime', { search: this.get('searchTextAnime') }, function(anime) {
+				return 1;
+				// return (anime.get('title').toLowerCase().indexOf(this.get('search_text_anime').toLowerCase()) > -1);
+			});		
+
+			this.set('animeResults', search_results);
+
+			return false;
+		},
+		select_anime: function(anime) {
+			this.set('selectedAnime', anime);
+		},
+
+		select_person: function(person) {
+			this.set('selectedPerson', person);
+		},
 	},
+
+	// Search anime
+	searchTextAnime: '',
+	animeResults: '',
+	selectedAnime: null,
+
+	// Search Person
+	searchTextPerson: '',
+	personResults: '',
+	selectedPerson: null,
+
 	// Genders
 	genders: [
 		"",
