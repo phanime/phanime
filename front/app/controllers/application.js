8d7cc4aa6dbbc8e7d47f87da588c7962fca46cf8
MaazAli <maaz_ali@outlook.com>
2014-07-25 21:31:27 -0700

    Added event handler for front page request invite button

--- front/app/controllers/application.js
+++ front/app/controllers/application.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import Notify from 'ember-notify';
 
 export default Ember.ObjectController.extend({
-
+	// Request invite email
+	email: null,
 	// Hide the nav bar / footer and other things if user is on the index page and isn't logged in
 	isLandingPage: function() {
 		if ( !this.get('session.isAuthenticated') && this.get('currentRouteName') === 'index') {
@@ -23,6 +24,13 @@ export default Ember.ObjectController.extend({
 			"http://31.media.tumblr.com/tumblr_m3q9vmuqGg1rozgayo1_500.gif",
 			"http://media-cache-ec0.pinimg.com/originals/13/9f/f5/139ff545b37593fcff57c0c9f676de17.jpg",
 			"http://media.giphy.com/media/13LQZoCE0Nysr6/giphy.gif",
+			"http://static.tumblr.com/qmeablg/6JAlyn4op/tumblr_layytg8cqf1qc2jhfo1_500.gif",
+			"http://media-cache-ec0.pinimg.com/originals/68/0b/69/680b69563aceba3df48b4483d007bce3.jpg",
+			"http://media.giphy.com/media/VUC9YdLSnKuJy/giphy.gif",
+			"http://media.giphy.com/media/OaHp43V1N4OvC/giphy.gif",
+			"http://data.whicdn.com/images/19521764/tumblr_luch96ogPv1qemssho1_500_large.gif",
+			"http://24.media.tumblr.com/f66cf98b45010359bea6ed008ffcd614/tumblr_n5eyqwMG1q1sf5vppo1_500.gif",
+			"http://i.imgur.com/6mOAx9v.gif",
 		];
 
 		var randGif = gifs[Math.floor(Math.random() * gifs.length)];
@@ -36,6 +44,22 @@ export default Ember.ObjectController.extend({
 	actions: {
 		trigger_search: function() {
 			this.transitionTo('search', this.get('search_query'));
-		}
+		},
+		requestInvite: function() {
+			var email = this.get('email');
+
+			if (email != '' && email != null) {
+				$.post('/api/v1/requestInvite', {email: email}, function(data) {
+
+				}).done(function() {
+					Notify.success('Invite request received.');
+				}).fail(function() {
+					Notify.warning('Something went wrong, invite request was not received.');
+				});
+
+
+				console.log(email);
+			}
+		},
 	}
 });
