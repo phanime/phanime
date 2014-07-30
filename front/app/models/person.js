0add8d741adbd7882f2ba39cd85fbb06290db3a9
MaazAli <maaz_ali@outlook.com>
2014-07-29 19:07:44 -0700

    Updated person page

--- front/app/models/person.js
+++ front/app/models/person.js
@@ -5,6 +5,11 @@ var attr = DS.attr,
 
 export default DS.Model.extend({
 	cover_photo: attr(),
+
+	cover_photo_url: function() {
+		return "http://cdn.phanime.com/images/people/cover/" + this.get('cover_photo');
+	}.property('cover_photo'),	
+
 	full_name: function() {
 		return this.get('first_name') + " " + this.get('last_name');
 	}.property('first_name', 'last_name'),
