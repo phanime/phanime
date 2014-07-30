a33aa2b770c0a924dc92899052c449372f485ea4
MaazAli <maaz_ali@outlook.com>
2014-07-29 19:07:44 -0700

    Updated person page

--- front/tests/unit/views/calendar-view-test.js
+++
@@ -1,9 +0,0 @@
-import { test, moduleFor } from 'ember-qunit';
-
-moduleFor('view:calendar-view', 'CalendarViewView');
-
-// Replace this with your real tests.
-test('it exists', function() {
-  var view = this.subject();
-  ok(view);
-});
