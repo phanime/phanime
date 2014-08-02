6e7421e3a2fba51bb6830c1392c4f8f92b1ab15b
MaazAli <maaz_ali@outlook.com>
2014-08-01 18:52:36 -0700

    Internal versions changed. Some changes made to ready it for release v0.0.1

--- phanime/Brocfile.js
+++ phanime/Brocfile.js
@@ -2,7 +2,16 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+	minifyCSS: {
+		enabled: true,
+		options: {}
+	},
+	minifyJS: {
+		enabled: true,
+		options: {}
+	}
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
