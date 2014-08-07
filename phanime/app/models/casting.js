76a233d823d5a9e30e44e01eb53f90444f08fdd3
MaazAli <maaz_ali@outlook.com>
2014-08-06 19:46:31 -0700

    Castings and Staff Members now display on the main page

--- phanime/app/models/casting.js
+++ phanime/app/models/casting.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 var attr = DS.attr,
 	belongsTo = DS.belongsTo;
@@ -7,6 +8,8 @@ export default DS.Model.extend({
 	language: attr(),
 	role: attr(),
 	person_id: belongsTo('person'),
+	person: Ember.computed.alias('person_id'),
 	character_id: belongsTo('character'),
+	character: Ember.computed.alias('character_id'),
 	anime_id: belongsTo('anime')
 });
