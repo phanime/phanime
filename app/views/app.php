60c9d54bcea97c879e128494ef219dca67d2ebaf
MaazAli <maaz_ali@outlook.com>
2014-08-10 18:11:57 -0700

    Changed end date to friendly date

--- app/views/app.php
+++ app/views/app.php
@@ -9,16 +9,16 @@
 
     <base href="/" />
 
-    <link rel="stylesheet" href="assets/vendor.css">
-    <link rel="stylesheet" href="assets/phanime.css">
+    <link rel="stylesheet" href="assets/vendor-75f589fb49f0c57c79e647241c1b4e49.css">
+    <link rel="stylesheet" href="assets/phanime-e1886cc3a6fed1bc09db4ba1d6d564d6.css">
   </head>
   <body>
     <script>
-      window.PhanimeENV = {"environment":"development","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{"LOG_ACTIVE_GENERATION":true,"LOG_VIEW_LOOKUPS":true},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"},"simple-auth":{"authorizer":"simple-auth-authorizer:oauth2-bearer"}};
+      window.PhanimeENV = {"environment":"production","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"},"simple-auth":{"authorizer":"simple-auth-authorizer:oauth2-bearer"}};
       window.EmberENV = window.PhanimeENV.EmberENV;
     </script>
-    <script src="assets/vendor.js"></script>
-    <script src="assets/phanime.js"></script>
+    <script src="assets/vendor-44f784f973f5e4769224203f8cca8395.js"></script>
+    <script src="assets/phanime-e17084e45ab05f7855f44be16ee834c4.js"></script>
     <script>
       window.Phanime = require('phanime/app')['default'].create(PhanimeENV.APP);
     </script>
