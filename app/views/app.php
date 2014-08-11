c7f27fcec9ceb18ce9dd54fb666970dfb7ef4f98
MaazAli <maaz_ali@outlook.com>
2014-08-10 18:05:13 -0700

    Removed add anime function from general application

--- app/views/app.php
+++ app/views/app.php
@@ -9,16 +9,16 @@
 
     <base href="/" />
 
-    <link rel="stylesheet" href="assets/vendor-75f589fb49f0c57c79e647241c1b4e49.css">
-    <link rel="stylesheet" href="assets/phanime-f239c0fffd13495960c73e4e39e9e1ea.css">
+    <link rel="stylesheet" href="assets/vendor.css">
+    <link rel="stylesheet" href="assets/phanime.css">
   </head>
   <body>
     <script>
-      window.PhanimeENV = {"environment":"production","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"},"simple-auth":{"authorizer":"simple-auth-authorizer:oauth2-bearer"}};
+      window.PhanimeENV = {"environment":"development","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{"LOG_ACTIVE_GENERATION":true,"LOG_VIEW_LOOKUPS":true},"simple-auth-oauth2":{"serverTokenEndpoint":"/api/v1/oauth/token"},"simple-auth":{"authorizer":"simple-auth-authorizer:oauth2-bearer"}};
       window.EmberENV = window.PhanimeENV.EmberENV;
     </script>
-    <script src="assets/vendor-44f784f973f5e4769224203f8cca8395.js"></script>
-    <script src="assets/phanime-7c683b7fd179771d8614a935c3309bb7.js"></script>
+    <script src="assets/vendor.js"></script>
+    <script src="assets/phanime.js"></script>
     <script>
       window.Phanime = require('phanime/app')['default'].create(PhanimeENV.APP);
     </script>
