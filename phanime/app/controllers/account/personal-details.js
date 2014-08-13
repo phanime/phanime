c066ce48fbef0161309c2bb918dfa39974499b0d
MaazAli <maaz_ali@outlook.com>
2014-08-12 17:25:20 -0700

    Changing the front end upload parameters

--- phanime/app/controllers/account/personal-details.js
+++ phanime/app/controllers/account/personal-details.js
@@ -7,7 +7,7 @@ export default Ember.ObjectController.extend({
 		this.get('session.currentUser').then(function(user) {
 			console.log(user);
 			console.log(user.get('username'));
-			self.set('extraParams', "&imageCategory=avatar&namePref=" + user.get('id') + "&imageFor=users");
+			self.set('extraParams', "&imageCategory=avatar&contentID=" + user.get('id') + "&imageFor=users");
 			//self.set('extraParams', '');
 		});
 	}.observes('extraParams').on('init'),
