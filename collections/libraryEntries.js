83dd15ec75bd434024f6dd30439cc1ca3c814bc9
MaazAli <maaz_ali@outlook.com>
2014-08-17 17:11:30 -0400

    Library Entries can now be added

--- collections/libraryEntries.js
+++ collections/libraryEntries.js
@@ -1,1 +1,16 @@
 LibraryEntries = new Meteor.Collection("libraryEntries");
+
+LibraryEntries.allow({
+
+	insert: function(userId, doc) {
+		// the user must be logged in, and the library entry must be created by the user
+		return (userId && doc.userId === userId);
+	},
+	update: function(userId, doc, fields, modifier) {
+		// can only change your own library entries
+		return doc.userId === userId;
+
+	}
+
+
+});
