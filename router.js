703edb5dc94a31cd291eda5a461d01a1ebb2fa84
MaazAli <maaz_ali@outlook.com>
2014-08-16 20:06:38 -0400

    Directory structure changed as well as login template added

--- router.js
+++ router.js
@@ -9,64 +9,13 @@ Router.map(function () {
 
 	this.route('anime', {path: '/anime/:slug', layoutTemplate: 'defaultLayout'});
 
-	// this.resource('anime', {path: 'anime/:slug'}, function() {
-	// 	// this.resource('episode', {path: '/episode/:episode_number'}, function() {
-	// 	// 	this.resource('video', { path: ':video_id' });
-	// 	// });
-	// 	this.route('add');
-	// });
-
-	// // Character related routes
-	// this.resource('characters', function() {
-	// 	this.route('add');
-	// });
-
-	// this.resource('character', { path: 'characters/:character_id/:character_slug' });
-
-	// // Producer related routes
-	// this.resource('producers', function() {
-	// 	this.route('add');
-	// });
-
-	// this.resource('producer', { path: 'producers/:producer_id/:producer_slug' });
-
-	// // Person related routes
-	// this.resource('people', function() {
-	// 	this.route('add');
-	// });
-
-	// this.resource('person', { path: 'people/:person_id/:person_slug' });
-	
-
-	// // Casting related routes
-	// this.resource('castings', function() {
-	// 	this.route('add');
-	// });
-
-	// // staffMember related routes
-	// this.resource('staffMembers', {path: 'staff-members'}, function() {
-	// 	this.route('add');
-	// });
-
-
-	// this.resource('account', function() {
-	// 	this.route('personalDetails', {path :'personal-details'});
-	// 	this.route('security');
-	// 	this.route('preferences');
-	// });
-
-	// // Users related routes
-	// this.resource('user', { path: 'users/:username' }, function() {
-	// 	this.route('library');
-	// });
-
 	// Search
 
 	this.route('search', {path: 'search/:query'});
 
 	// Login
 
-	this.route('login');
+	this.route('login', {layoutTemplate: 'defaultLayout'});
 
 
 	// 404 not found route
@@ -74,3 +23,23 @@ Router.map(function () {
 	this.route('fourOhFour', {path: "*path"});
 
 });
+
+////////////////
+// BeforeHooks
+////////////////
+// I use an object that contains all before hooks
+var routerBeforeHooks = {
+	isLoggedIn: function(pause) {
+		if (!(Meteor.loggingIn() || Meteor.user())) {
+			this.render('login');
+			pause();
+		}
+	}
+}
+
+// (Global) Before hooks for any route
+Router.onBeforeAction(routerBeforeHooks.isLoggedIn);
+
+// // Before hooks for specific routes
+// // Must be equal to the route names of the Iron Router route map
+// Router.before(IR_BeforeHooks.isLoggedIn, {only: ['userAreaA', 'userAreaB']});
