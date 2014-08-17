876e72aee29513aac78dd858ea2211165601dc04
MaazAli <maaz_ali@outlook.com>
2014-08-17 13:07:22 -0400

    Menu items updated, episodes added to anime

--- server/publications/anime.js
+++ server/publications/anime.js
@@ -5,12 +5,24 @@
 
 // Return anime by it's _id, no related data included
 Meteor.publish("animeById", function(animeId) {
-	return Anime.find({_id: animeId});
+	return [
+		Anime.findOne({_id: animeId}),
+		Episodes.find({animeId: animeId})
+	];
 });
 
 
 // Return anime by it's slug with all related anime data
 
 Meteor.publish("animeBySlug", function(animeSlug) {
-	return Anime.find({slug: animeSlug});
+	var anime = Anime.findOne({slug: animeSlug});
+	var castings = Castings.find({animeId: anime._id});
+
+	// Get characters
+
+	return [
+		Anime.find({slug: animeSlug}),
+		Episodes.find({animeId: anime._id}),
+		Castings.find({animeId: anime._id})
+	]
 });
