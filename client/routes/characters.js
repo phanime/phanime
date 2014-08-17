a1abf0271f45b9f987d89de309835a67e8a0df8e
MaazAli <maaz_ali@outlook.com>
2014-08-17 13:34:42 -0400

    Character pages data connected

---
+++ client/routes/characters.js
@@ -0,0 +1,19 @@
+CharactersController = RouteController.extend({
+	
+	onBeforeAction: function () {
+		console.log('Thing are going well');
+	},
+
+	onAfterAction: function () {
+		console.log('Everything worked');
+	},
+
+	waitOn: function () {
+		return Meteor.subscribe('characters');
+	},
+
+	data: function () {
+		return Characters.find();
+	}
+
+});
