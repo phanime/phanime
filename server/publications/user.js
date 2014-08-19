e3c245ed3dd97e1d0cfca0c2595a029ee4be6720
MaazAli <maaz_ali@outlook.com>
2014-08-18 23:48:17 -0400

    Half assed implementation of library

--- server/publications/user.js
+++ server/publications/user.js
@@ -4,3 +4,11 @@ Meteor.publish('user', function(username) {
 	return Meteor.users.find({username: username});
 });
 
+Meteor.publish('userWithLibraryEntries', function(username) {
+	var user = Meteor.users.findOne({username: username});
+
+	return [
+		Meteor.users.find({username: username}),
+		LibraryEntries.find({userId: user._id})
+	];
+})
