594f0ba08b1f37d9bd7273f0783caa1434eb15f4
MaazAli <maaz_ali@outlook.com>
2014-08-19 21:59:21 -0400

    Added a template helper as well as styled the anime library a bit

---
+++ client/template-helpers/getPercentage.js
@@ -0,0 +1,9 @@
+UI.registerHelper("getPercentage", function(num, den) {
+	if (!den) {
+		return '0%';
+	} else if (!num) {
+		return '0%';
+	} else {
+		return ((num/den)*100).toFixed(2) + "%";
+	} 
+});
