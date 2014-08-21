ea4deff210d463f4c171582685ea99005e33be72
MaazAli <maaz_ali@outlook.com>
2014-08-21 19:23:17 -0400

    People add form added

--- client/views/user/userLibrary.js
+++ client/views/user/userLibrary.js
@@ -22,41 +22,38 @@ Template.userLibrary.watchStatuses = [
 Template.userLibrary.events({
 
 	'click .status-item' : function(event, template) {
-		// For some awkward reason, unable to use 
-		// $(this).html() or $(this).text() .. will
-		// need to examine this further, for now
-		// we'll do with the .join
+
 		var status = $(event.target).text();
 		
 		console.log(Meteor.userId());
 
 		// Set the current library entry
 		var anime = template.data;
-		var libraryEntry = $(event.target).find('.libraryEntryId').val();
+		var libraryEntry = $(event.target).parents('.libraryEntryId');
 		console.log(libraryEntry);
 
 
-		if (currentEntry) {
-			// libraryEntry exists for the current user
+		// if (currentEntry) {
+		// 	// libraryEntry exists for the current user
 
-			// If the user has selected remove as status
-			// then we should delete their library entry
-			if (status === 'Remove') {
-				LibraryEntries.remove({_id: currentEntry._id});
-			}
+		// 	// If the user has selected remove as status
+		// 	// then we should delete their library entry
+		// 	if (status === 'Remove') {
+		// 		LibraryEntries.remove({_id: currentEntry._id});
+		// 	}
 
-			// But make sure the status is different
-			if (status !== currentEntry.status) {
+		// 	// But make sure the status is different
+		// 	if (status !== currentEntry.status) {
 
-				LibraryEntries.update({_id: currentEntry._id}, {$set: {
-					status: status, 
-					episodesSeen: (anime.totalEpisodes && status === 'Completed' ? currentEntry.anime.totalEpisodes : null)
-				}});
-				Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated');
-			} else {
-				console.log('Statuses same, don\'t update');
-			}
-		}
+		// 		LibraryEntries.update({_id: currentEntry._id}, {$set: {
+		// 			status: status, 
+		// 			episodesSeen: (anime.totalEpisodes && status === 'Completed' ? currentEntry.anime.totalEpisodes : null)
+		// 		}});
+		// 		Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated');
+		// 	} else {
+		// 		console.log('Statuses same, don\'t update');
+		// 	}
+		// }
 	}
 
 });
--- collections/people.js
+++ collections/people.js
@@ -13,3 +13,53 @@ People = new Meteor.Collection("people", {
 		return doc;
 	}
 });
+
+PeopleSchema = new SimpleSchema({
+
+	coverImage: {
+		type: String,
+		label: "Cover Image",
+	},
+	firstName: {
+		type: String,
+		label: "First Name"
+	},
+	lastName: {
+		type: String,
+		label: "Last Name"
+	},
+	givenName: {
+		type: String,
+		label: "Given Name"
+	},
+	familyName: {
+		type: String,
+		label: "Family Name"
+	},
+	gender: {
+		type: String,
+		label: "Gender",
+	},
+	birthDate: {
+		type: Date,
+		label: "Birthdate"
+	},
+	website: {
+		type: String,
+		label: "Website",
+	},
+	birthPlace: {
+		type: String,
+		label: "Birth Place"
+	},
+	bloodType: {
+		type: String,
+		label: "Blood Type"
+	},
+	otherInfo: {
+		type: String,
+		label: "Other Info"
+	}
+
+
+});
