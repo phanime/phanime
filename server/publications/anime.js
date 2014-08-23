15ad1fb4d4686e0069fa53a9b791ad77dfce31cc
MaazAli <maaz_ali@outlook.com>
2014-08-23 17:41:00 -0400

    Anime relations added

--- server/publications/anime.js
+++ server/publications/anime.js
@@ -5,25 +5,92 @@
 
 // Return anime by it's _id, no related data included
 Meteor.publish("animeById", function(animeId) {
-	return [
-		Anime.findOne({_id: animeId}),
-		Episodes.find({animeId: animeId})
-	];
+	return {
+		find: function() {
+			return Anime.find({slug: animeId});
+		},
+		children: [
+			{
+				find: function(anime) {
+					return Episodes.find({animeId: anime._id});
+				} 
+			},
+			{
+				find: function(anime) {
+					return Castings.find({animeId: anime._id});
+				},
+				children: [
+					{
+						find: function(casting, anime) {
+							return Characters.find({_id: casting.characterId});
+						}
+					},
+					{
+						find: function(casting, anime) {
+							return People.find({_id: casting.personId});
+						}
+					}
+				]  
+			},
+			{
+				find: function(anime) {
+					return StaffMembers.find({animeId: anime._id});
+				},
+				children: [
+					{
+						find: function(staffMember, anime) {
+							return People.find({_id: staffMember.personId});
+						}
+					}
+				] 
+			}
+
+		]
+	};
 });
 
+Meteor.publishComposite("animeBySlug", function(animeSlug) {
 
-// Return anime by it's slug with all related anime data
+	return {
+		find: function() {
+			return Anime.find({slug: animeSlug});
+		},
+		children: [
+			{
+				find: function(anime) {
+					return Episodes.find({animeId: anime._id});
+				} 
+			},
+			{
+				find: function(anime) {
+					return Castings.find({animeId: anime._id});
+				},
+				children: [
+					{
+						find: function(casting, anime) {
+							return Characters.find({_id: casting.characterId});
+						}
+					},
+					{
+						find: function(casting, anime) {
+							return People.find({_id: casting.personId});
+						}
+					}
+				]  
+			},
+			{
+				find: function(anime) {
+					return StaffMembers.find({animeId: anime._id});
+				},
+				children: [
+					{
+						find: function(staffMember, anime) {
+							return People.find({_id: staffMember.personId});
+						}
+					}
+				] 
+			}
 
-Meteor.publish("animeBySlug", function(animeSlug) {
-	var anime = Anime.findOne({slug: animeSlug});
-	//var castings = Castings.find({animeId: anime._id});
-
-	// Get characters
-
-	return [
-		Anime.find({slug: animeSlug}),
-		Episodes.find({animeId: anime._id}),
-		Castings.find({animeId: anime._id}),
-		LibraryEntries.find({animeId: anime._id, userId: this.userId})
-	];
+		]
+	};
 });
