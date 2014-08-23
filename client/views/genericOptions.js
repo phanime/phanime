e081480350628940f9071291945464937b6da588
MaazAli <maaz_ali@outlook.com>
2014-08-23 01:40:10 -0400

    Added generic options for content additions

---
+++ client/views/genericOptions.js
@@ -0,0 +1,27 @@
+UI.registerHelper('animeAll', function() {
+	return Anime.find().map(function(anime) {
+		return {
+			label: anime.canonicalTitle,
+			value: anime._id
+		};
+	});
+});
+
+UI.registerHelper('peopleAll', function() {
+	return People.find().map(function(person) {
+		return {
+			label: person.fullName,
+			value: person._id
+		};
+	});
+});
+
+
+UI.registerHelper('charactersAll', function() {
+	return Characters.find().map(function(character) {
+		return {
+			label: character.fullName,
+			value: character._id
+		};
+	});
+});
