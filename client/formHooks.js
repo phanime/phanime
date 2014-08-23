446d9b25fb5a84ab210121bdac848cad85716960
MaazAli <maaz_ali@outlook.com>
2014-08-23 15:04:18 -0400

    Castings form updated again, fixed a bug

---
+++ client/formHooks.js
@@ -0,0 +1,12 @@
+// AutoForm.hooks({
+//   insertCasting: {
+//     onSubmit: function (insertDoc, updateDoc, currentDoc) {
+//       if (customHandler(insertDoc)) {
+//         this.resetForm();
+//         this.done();
+//       }
+//       console.log('Submitted');
+//       return false;
+//     }
+//   }
+// });
---
+++ server/methods/castings.js
@@ -0,0 +1,5 @@
+Meteor.methods({
+	createCasting: function(doc) {
+	    console.log(doc);
+	}
+});
