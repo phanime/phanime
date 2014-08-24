0d0ccb74e830bae3a0191496dcc579b896642d2f
MaazAli <maaz_ali@outlook.com>
2014-08-23 20:16:36 -0400

    Added proper create methods

--- server/methods/anime.js
+++ server/methods/anime.js
@@ -1,5 +1,21 @@
 Meteor.methods({
-    createAnime: function(doc) {
-        console.log(doc);
-    }
+    createAnime: function(anime) {
+
+		// Ensure integerity of data
+		check(anime, AnimeSchema);
+
+		// Get auto values
+		anime.slug = getSlug(anime.canonicalTitle);
+		anime.createdAt = new Date();
+		anime.updatedAt = new Date();
+
+		// Insert the document into the database
+		Anime.insert(anime, function(error, _id) {
+			console.log(_id);
+		});
+
+		console.log(anime);
+
+
+	}
 });
--- server/methods/castings.js
+++ server/methods/castings.js
@@ -1,5 +1,15 @@
 Meteor.methods({
-	createCasting: function(doc) {
-	    console.log(doc);
+	createCasting: function(casting) {
+	    
+	    check(casting, CastingsSchema);
+
+	    // Auto value
+	    casting.createdAt = new Date();
+
+	    Castings.insert(casting, function(error, _id) {
+	    	console.log(_id);
+	    });
+
+	    console.log(casting);
 	}
 });
