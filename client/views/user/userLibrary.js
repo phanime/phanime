722d90fb7d788f2b227ee7b5de1697fe58c1454b
MaazAli <maaz_ali@outlook.com>
2014-08-24 21:18:03 -0400

    Library Entry statuses now update properly

--- client/views/user/userLibrary.js
+++ client/views/user/userLibrary.js
@@ -1,15 +1,15 @@
-Template.userLibrary.rendered = function() {
-	$('.libraryEntryCard').popover({
-		html: true,
-		placement: 'auto right',
-		content: function() {
-			return $(this).find('.libraryEntryForm').html();
-		},
-		trigger: 'click'
-	});
-};
+// Template.userLibrary.rendered = function() {
+// 	$('.libraryEntryCard').popover({
+// 		html: true,
+// 		placement: 'auto right',
+// 		content: function() {
+// 			return $(this).find('.libraryEntryForm').html();
+// 		},
+// 		trigger: 'click'
+// 	});
+// };
 
-Template.userLibrary.watchStatuses = [
+Template.libraryEntryCard.watchStatuses = [
 	"Watching",
 	"Completed",
 	"Plan to watch",
@@ -19,41 +19,38 @@ Template.userLibrary.watchStatuses = [
 ];
 
 
-Template.userLibrary.events({
+Template.libraryEntryCard.events({
 
 	'click .status-item' : function(event, template) {
 
 		var status = $(event.target).text();
 		
-		console.log(Meteor.userId());
 
 		// Set the current library entry
-		var anime = template.data;
-		var libraryEntry = $(event.target).parents('.libraryEntryId');
-		console.log(libraryEntry);
+		var libraryEntry = template.data;
+		var anime = libraryEntry.anime;
 
+		if (libraryEntry) {
+			// libraryEntry exists for the current user
 
-		// if (currentEntry) {
-		// 	// libraryEntry exists for the current user
+			// If the user has selected remove as status
+			// then we should delete their library entry
+			if (status === 'Remove') {
+				LibraryEntries.remove({_id: libraryEntry._id});
+			}
 
-		// 	// If the user has selected remove as status
-		// 	// then we should delete their library entry
-		// 	if (status === 'Remove') {
-		// 		LibraryEntries.remove({_id: currentEntry._id});
-		// 	}
+			// But make sure the status is different
+			if (status !== libraryEntry.status) {
 
-		// 	// But make sure the status is different
-		// 	if (status !== currentEntry.status) {
-
-		// 		LibraryEntries.update({_id: currentEntry._id}, {$set: {
-		// 			status: status, 
-		// 			episodesSeen: (anime.totalEpisodes && status === 'Completed' ? currentEntry.anime.totalEpisodes : null)
-		// 		}});
-		// 		Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated');
-		// 	} else {
-		// 		console.log('Statuses same, don\'t update');
-		// 	}
-		// }
+				LibraryEntries.update({_id: libraryEntry._id}, {$set: {
+					status: status, 
+					episodesSeen: (anime.totalEpisodes && status === 'Completed' ? anime.totalEpisodes : null)
+				}});
+				Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated');
+			} else {
+				console.log('Statuses same, don\'t update');
+			}
+		}
 	}
 
 });
