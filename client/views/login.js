24ae776cf885f43939f33d2d8d40a0285719342e
MaazAli <maaz_ali@outlook.com>
2014-08-25 23:04:56 -0400

    Made the login function a little smarter

--- client/views/login.js
+++ client/views/login.js
@@ -9,6 +9,18 @@ Template.login.events({
 
 		Meteor.loginWithPassword(identification, password, function(error) {
 			console.log(error);
+			var currentRouteName = Router.current().route.name;
+
+			if (error) {
+				Notifications.error('Login Unsuccessful', 'Username or password is invalid');
+			} else {
+				// Redirect the user to index page if login successful only if they are on the login route
+				if (currentRouteName === "login") {
+					Router.go('index');
+				}
+
+			}
+
 		});
 
 	}
