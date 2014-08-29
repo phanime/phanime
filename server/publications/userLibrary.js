5a039782ed4f94d54aecf0bd64a98ba5cefa07b7
MaazAli <maaz_ali@outlook.com>
2014-08-28 20:21:30 -0400

    Limited library entries on the index page to plan to watch or watching

--- server/publications/userLibrary.js
+++ server/publications/userLibrary.js
@@ -18,3 +18,18 @@ Meteor.publishComposite('libraryEntriesLatest', function() {
 		]
 	}; 
 });
+
+Meteor.publishComposite('libraryEntriesIndexLatest', function() {
+	return {
+		find: function() {
+			return LibraryEntries.find({userId: this.userId, $or : [{status: 'Watching'}, {status: 'Plan to watch'}]}, {limit: 6});
+		},
+		children: [
+			{
+				find: function(libraryEntry) {
+					return Anime.find({_id: libraryEntry.animeId});
+				}
+			}
+		]
+	}; 
+});
