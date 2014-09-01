27736aa26e03be0d6400ec2dee5c652160220e2d
MaazAli <maaz_ali@outlook.com>
2014-09-01 13:47:20 -0400

    Reviews are now added properly

--- client/views/review/reviewAdd.js
+++ client/views/review/reviewAdd.js
@@ -9,6 +9,9 @@ Template.reviewAdd.events({
 
 	'click #reviewAddBtn' : function (event, template) {
 
+		var anime = template.data;
+
+		// Setup 
 		var reviewContent = $('#reviewContent').val().trim();
 		var reviewSummary = $('#reviewSummary').val().trim();
 
@@ -19,12 +22,35 @@ Template.reviewAdd.events({
 		var enjoymentRating = $('#soundRating').rateit('value');
 		var overallRating = $('#overallRating').rateit('value');
 
+		// Checks done in insert rules in the collection
+
+		var review = {
+			animeId: anime._id,
+			userId: Meteor.userId(),
+			content: reviewContent,
+			summary: reviewSummary,
+			storyRating: storyRating,
+			animationRating: animationRating,
+			characterRating: characterRating,
+			soundRating: soundRating,
+			enjoymentRating: enjoymentRating,
+			overallRating: overallRating
+		};
+
+		Reviews.insert(review, function(error, result) {
+			console.log(error);
+			console.log(result);
+
+			if (error) {
+				console.log('Stuff');
+				Notifications.error('Failed to Publish Review', 'Looks like something went wrong! Please ensure you have the required fields filled out');
+				//Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated to ' + status);
+			} else {
+				Notifications.success('Successfully Published Review', 'Your review should show up on the anime page in a few moments');
+			}
 
 
-		console.log(reviewContent);
-		console.log(reviewSummary);
-		console.log(storyRating);
-
+		});
 
 
 
