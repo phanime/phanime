5c067134b7aa2c82ee1aa58666a7934a236fa350
MaazAli <maaz_ali@outlook.com>
2014-09-01 13:47:20 -0400

    Reviews are now added properly

--- collections/reviews.js
+++ collections/reviews.js
@@ -5,7 +5,18 @@ Reviews.allow({
 
 	insert: function(userId, doc) {
 		// the user must be logged in, and the review must be created by the user
-		return (userId && doc.userId === userId);
+		var userCondition = (userId && doc.userId === userId);
+
+		var contentCondition = (doc.content && doc.content.length > 10 && doc.summary && doc.summary.length <= 140 );
+
+		var ratingCondition = (doc.overallRating >= 1 && doc.overallRating <= 10);
+
+		console.log(userCondition);
+		console.log(contentCondition);
+		console.log(ratingCondition);
+		
+		return userCondition && contentCondition && ratingCondition;
+
 	},
 	update: function(userId, doc, fields, modifier) {
 
