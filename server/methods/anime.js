bb29aaef95a9b803f2e3d1cbbd63a489966ccf35
MaazAli <maaz_ali@outlook.com>
2014-09-02 15:56:16 -0400

    Unique check added for Anime

--- server/methods/anime.js
+++ server/methods/anime.js
@@ -8,13 +8,34 @@ Meteor.methods({
 		anime.slug = getSlug(anime.canonicalTitle);
 		anime.createdAt = new Date();
 
+		// Ensure uniqueness
+		var titleCheck = Anime.findOne({canonicalTitle: anime.canonicalTitle});
+		var slugCheck = Anime.findOne({slug: anime.slug});
 
-		// Insert the document into the database
-		Anime.insert(anime, function(error, _id) {
-			console.log(_id);
-		});
+		var uniqueCondition;
 
-		console.log(anime);
+		console.log(titleCheck);
+		console.log(slugCheck);
+
+		if (titleCheck || slugCheck) {
+			uniqueCondition = false;
+		} else {
+			uniqueCondition = true;
+		}
+
+		if (uniqueCondition) {
+
+
+			// Insert the document into the database
+			Anime.insert(anime, function(error, _id) {
+				console.log(_id);
+			});
+
+			console.log(anime);
+
+		} else {
+			console.log ('Anime is not unique!');
+		}
 
 
 	}
