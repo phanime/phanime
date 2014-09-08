c0a417e925dacb10b1daba7980a1da55b84782df
MaazAli <maaz_ali@outlook.com>
2014-09-08 19:18:00 -0400

    Fixed up the start date / end date text

--- client/views/anime/anime.js
+++ client/views/anime/anime.js
@@ -19,6 +19,39 @@ Template.anime.rendered = function() {
 
 };
 
+
+Template.anime.airingText = function() {
+
+	console.log(moment(this.startDate).year());
+	console.log(this.endDate);
+	console.log(moment(this.endDate).year());
+
+	if (moment(this.startDate).year() && moment(this.endDate).year()) {
+	
+		// Both dates are defined, now we just need to figure out if the anime is airing / going to air / finished airing
+
+		// Currently airing
+		if (moment().isAfter(this.startDate) && moment().isBefore(this.endDate)) {
+			return "Airing from";
+		} else if (moment().isBefore(this.startDate)) {
+			return "Will air from ";
+		} else if (moment().isAfter(this.endDate)) {
+			return "Aired from";
+		}
+
+	} else if (!moment(this.endDate).year()) {
+
+		// Now we should check if there is a this.endDate field and if it's just not null
+		if (this.endDate) {
+			return "Aired from";
+		} else {
+			return "Aired on";
+		}
+
+	}
+
+};
+
 Template.anime.destroyed = function() {
 	$('.block.scroll-ready').mCustomScrollbar('destroy');
 };
