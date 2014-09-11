684a510f3374be05a13b339a875194c464cd7b00
MaazAli <maaz_ali@outlook.com>
2014-09-10 21:50:54 -0400

    Some cleaning up done before pushing it to production

--- collections/activity.js
+++ collections/activity.js
@@ -14,13 +14,3 @@ Activity.libraryEntryFields = function(type, contentId, whatChanged, newValue) {
 	return libraryEntry;
 
 };
-
-// Activity.postFields = function(type, posterId, content) {
-// 	var post = {
-// 		type: type, // can either be profilePost or statusUpdate 
-// 		posterId: posterId, // if type is statusUpdate posterId would be equal to userId
-// 		content: content
-// 	};
-
-// 	return post;
-// };
