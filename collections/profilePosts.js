322638cc0a1629488bcb3bc5bef177c459afceee
MaazAli <maaz_ali@outlook.com>
2014-09-10 21:08:29 -0400

    Profile posts insert,update,remove rules added

--- collections/profilePosts.js
+++ collections/profilePosts.js
@@ -13,3 +13,25 @@ ProfilePosts.postFields = function(statusUpdate, posterId, content) {
 
 	return post;
 };
+
+ProfilePosts.allow({
+
+	insert: function(userId, doc) {
+		// the user must be logged in, and the profile post must be created by the user
+		return (userId && doc.userId === userId);
+	},
+	update: function(userId, doc, fields, modifier) {
+
+		// can only update their own profile posts
+		return doc.userId === userId;
+
+	},
+	remove: function(userId, doc) {
+
+		// can only remove profile posts that they created
+		return doc.userId === userId;
+
+	}
+
+
+});
