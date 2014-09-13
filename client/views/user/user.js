4193cdde82737d1c1cd6d07684eef4097af9d736
MaazAli <maaz_ali@outlook.com>
2014-09-13 01:26:25 -0400

    Created a highcharts package and defined a helper for testing

--- client/views/user/user.js
+++ client/views/user/user.js
@@ -24,3 +24,78 @@ Template.userSummary.events({
 	}
 	
 });
+
+
+
+
+// Charts data 
+// Get user's top genres
+
+Template.user.topGenresChart = function() {
+	return {
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
+		title: {
+			text: this.username + "'s top genres"
+		},
+		tooltip: {
+			pointFormat: '<b>{point.percentage:.1f}%</b>'
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					},
+					connectorColor: 'silver'
+				}
+			}
+		},
+		series: [{
+			type: 'pie',
+			name: 'genre',
+			data: [
+				['Adventure',   45.0],
+				['Action',       26.8],
+				['Ecchi',   12.8],
+				['Comedy',    8.5],
+				['Yuri',     6.2]
+			]
+		}]
+	};
+};
+// return {
+// 		chart: {
+// 			plotBackgroundColor: null,
+// 			plotBorderWidth: null,
+// 			plotShadow: false
+// 		},
+// 		title: {
+// 			text: this.username + "'s top genres"
+// 		},
+// 		tooltip: {
+// 			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+// 		},
+// 		plotOptions: {
+// 			pie: {
+// 				allowPointSelect: true,
+// 				cursor: 'pointer',
+// 				dataLabels: {
+// 					enabled: true,
+// 					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+// 					style: {
+// 						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+// 					},
+// 					connectorColor: 'silver'
+// 				}
+// 			}
+// 		}
+// 	};
+// };
