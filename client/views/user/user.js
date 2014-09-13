eed3b12832fb25dfebcb90f13e56ad61b105788d
MaazAli <maaz_ali@outlook.com>
2014-09-13 15:05:56 -0400

    Setup a backend for grabbing charts data, top genres for a specific user method added

--- client/views/user/user.js
+++ client/views/user/user.js
@@ -31,7 +31,74 @@ Template.userSummary.events({
 // Charts data 
 // Get user's top genres
 
+
+// Creating reactive variables
+Template.user.created = function() {
+
+	this.topGenresChartObject = new ReactiveVar({
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
+		title: {
+			text: this.username + "'s top genres"
+		},
+		tooltip: {
+			pointFormat: '<b>{point.percentage:.1f}%</b>'
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					},
+					connectorColor: 'silver'
+				}
+			}
+		},
+		series: [{
+			type: 'pie',
+			name: 'genre',
+			data: [
+				['Adventure',   45.0],
+				['Action',       26.8],
+				['Ecchi',   12.8],
+				['Comedy',    8.5],
+				['Yuri',     6.2]
+			]
+		}]
+	});
+
+};
+
+Template.user.topGenresChartObject = function() {
+
+	var template = Template.instance();
+	return template.topGenresChartObject.get();
+
+};
+
 Template.user.topGenresChart = function() {
+	var template = Template.instance();
+
+	// var oldObject = template.topGenresChartObject.get();
+
+	// oldObject.series[0]['type'] = 'bar';
+	// template.topGenresChartObject.set(oldObject);
+
+	// console.log(oldObject);
+
+
+	Meteor.call('topGenresChartData', this._id, function(error, result) {
+		console.log(error);
+		console.log(result);
+	});
+
 	return {
 		chart: {
 			plotBackgroundColor: null,
@@ -71,31 +138,3 @@ Template.user.topGenresChart = function() {
 		}]
 	};
 };
-// return {
-// 		chart: {
-// 			plotBackgroundColor: null,
-// 			plotBorderWidth: null,
-// 			plotShadow: false
-// 		},
-// 		title: {
-// 			text: this.username + "'s top genres"
-// 		},
-// 		tooltip: {
-// 			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-// 		},
-// 		plotOptions: {
-// 			pie: {
-// 				allowPointSelect: true,
-// 				cursor: 'pointer',
-// 				dataLabels: {
-// 					enabled: true,
-// 					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-// 					style: {
-// 						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-// 					},
-// 					connectorColor: 'silver'
-// 				}
-// 			}
-// 		}
-// 	};
-// };
