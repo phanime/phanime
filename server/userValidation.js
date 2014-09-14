a0a4a15ce4016d924f77c9c98b7125bbd8db56eb
MaazAli <maaz_ali@outlook.com>
2014-09-14 00:09:40 -0400

    Added a proper username regex check when registering

--- server/userValidation.js
+++ server/userValidation.js
@@ -3,9 +3,17 @@ Accounts.validateNewUser(function(user) {
 	var isGood = false;
 	console.log(user);
 
+	var usernameRegex = /^[a-zA-Z0-9_]+$/;
+
 	// Check user name
 	if (user.username && user.username.length >= 3 && user.username.length <= 30) {
-		isGood = true;
+		// Check that username doesn't have any spaces 
+		if (usernameRegex.test(user.username)) {
+			isGood = true;
+		} else {
+			throw new Meteor.Error(403, "Username can only contain alphanumeric characters and underscores");
+		}
+		
 	} else {
 		isGood = false;
 		throw new Meteor.Error(403, "Username must be between 3 and 30 characters in length");
