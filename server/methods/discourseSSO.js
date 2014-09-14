a71500ae1dd1757d27dd124119b9f0c8152cb19f
MaazAli <maaz_ali@outlook.com>
2014-09-13 22:16:10 -0400

    Half baked sso integration with phanime's discourse instance

--- server/methods/discourseSSO.js
+++ server/methods/discourseSSO.js
@@ -1,11 +1,26 @@
 Meteor.methods({
-	discourseSSO: function(payload, sig) {
-		var sso = new discourse_sso('6hkLiMrwyC7sB4JPm1Vu');
+	discourseSSO: function(payload, sig, user) {
+		var sso = new discourse_sso(privateSiteSettings.discourseSSOKey);
 		var nonce;
 		// validate sig with payload
-		if (sso.validat(payload, sig)) {
+		if (sso.validate(payload, sig)) {
 			var nonce = sso.getNonce(payload);
 			console.log(nonce);
+
+			var userParams = {
+				"nonce": nonce,
+				"external_id": user._id,
+				"email": user.emails[0].address, // we just grab the first address
+				"username": user.username,
+				"name": (user.profile) ? user.profile.name : ''  
+			};
+
+			var queryParams = sso.buildLoginString(userParams);
+			console.log(queryParams);
+			var returnUrl = "http://community.phanime.com/session/sso_login?" + queryParams;
+
+
+			return returnUrl;
 		}
-	};
+	}
 });
