82a0d054aab1de3e625112f337275f18416a61dd
MaazAli <maaz_ali@outlook.com>
2014-09-14 13:16:58 -0400

    A discourse sso auth medium (community/sso) route setup

--- collections/notifications.js
+++ collections/notifications.js
@@ -1,21 +1,21 @@
-Notifications = new Meteor.Collection('notifications');
+// Notifications = new Meteor.Collection('notifications');
 
 
-Notifications.allow({
-	insert: function(userId, doc) {
-		// We want the notifications to be sent only through
-		// a meteor method
-		return false;
-	}
-});
+// Notifications.allow({
+// 	insert: function(userId, doc) {
+// 		// We want the notifications to be sent only through
+// 		// a meteor method
+// 		return false;
+// 	}
+// });
 
-createNotification = function(event, properties, userToNotify) {
-	var notification = {
-		event: event,
-		userId: userToNotify,
-		properties: properties,
-		read: false
-	};
+// createNotification = function(event, properties, userToNotify) {
+// 	var notification = {
+// 		event: event,
+// 		userId: userToNotify,
+// 		properties: properties,
+// 		read: false
+// 	};
 
-	return notification;
-};
+// 	return notification;
+// };
