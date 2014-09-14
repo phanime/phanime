c9318254c9595218120c70d39b6432edd6adc42e
MaazAli <maaz_ali@outlook.com>
2014-09-14 13:48:25 -0400

    Normal discourse login flow is now functional when the user is logged in on the main app (which should be the general use case))

--- client/routes/authentication/communitySSO.js
+++ client/routes/authentication/communitySSO.js
@@ -1,18 +1,30 @@
 CommunitySSOController = RouteController.extend({
-	
+	template: 'loading',
 	onBeforeAction: function () {
-		SEO.set({
-			title: siteSettings.getFullTitle("Sign In"),
-			og: {
-				'title' : siteSettings.getFullTitle("Sign In"),
+		// If the user is logged in
+		// then we do the authentication for discourse
+		// else we send them to the sign in page with the proper query parameters
+		var params = this.params;
+		var user;
+		if (Meteor.user()) {
+			console.log(params);
+			if (params.sso && params.sig) {
+				user = Meteor.user();
+
+				user.avatarImageUrl = user.avatarImageUrl();
+				
+				// Temporary way to send the user to the right place after verification
+				Meteor.call('discourseSSO', params.sso, params.sig, user, function (error, result) {
+					console.log(result);
+					if (result) {
+						// We want to attach a return path in this login since
+						// the user is logging in from main application and has no 
+						// idea that they are also being authenticated for discourse
+						window.location = result;
+					}
+				});				
 			}
-		});
-	},
-	data: function() {
-		return {
-			sso: this.params.sso,
-			sig: this.params.sig
-		};
+		}
 	}
 
 });
