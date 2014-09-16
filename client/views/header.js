a4886f478281c216475250ff2f4e7c0989d82dfe
MaazAli <maaz_ali@outlook.com>
2014-09-15 21:32:45 -0400

    General alerts added to the front end, their user friendly text is still under workds

--- client/views/header.js
+++ client/views/header.js
@@ -4,3 +4,42 @@ Template.header.events({
 		return false;
 	}
 });
+
+Template.header.created = function() {
+
+	// Init with 0 and then have it update in rendered
+	this.unreadAlertCount = new ReactiveVar(0);
+
+};
+
+
+
+// We're setting these in 
+Template.header.alerts = function() {
+	var alerts = Alerts.find({userId: Meteor.userId(), read: false});
+	var template = Template.instance();
+
+	
+	template.unreadAlertCount.set(alerts.count());
+
+	return alerts;
+
+};
+
+Template.header.unreadAlertCount = function() {
+	var template = Template.instance();
+	return template.unreadAlertCount.get();
+};
+
+Template.header.alertsClass = function() {
+
+	var unreadAlertCount = Template.instance().unreadAlertCount.get();
+
+	if (unreadAlertCount === 0) {
+		return 'fa-bell-o';
+	} else {
+		return 'fa-bell';
+	}
+
+};
+
--- collections/alerts.js
+++ collections/alerts.js
@@ -7,3 +7,34 @@ Alerts.allow({
 		return false;
 	}
 });
+
+
+Alerts.helpers({
+
+
+	userFriendlyText: function() {
+		
+		var userFriendlyText;
+
+		switch (this.event) {
+
+			case "userFollow": 
+
+				var routerPath = Router.routes['user'].path({username: 'Maaz'});	
+				userFriendlyText = "Someone followed you, visit their <a href='" + routerPath + "'>profile</a>";
+
+				break;
+			case "userProfilePost":
+
+				userFriendlyText = 'Someone posted on your profile';
+
+				break;
+		}
+
+
+		return userFriendlyText;
+
+	}
+
+
+});
