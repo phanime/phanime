5e655e45ad3a9e78c035a1c5009b9e630ce84d36
MaazAli <maaz_ali@outlook.com>
2014-09-17 14:10:27 -0400

    Request invites now work properly

--- server/methods/emails.js
+++ server/methods/emails.js
@@ -3,11 +3,13 @@ Meteor.methods({
 	requestInviteEmail: function(email, code) {
 
 		// Extremely awkward way to generate the message atm
+		// But it'll do for now.
 		var message = '<h1>Discover Anime like Never Before</h1><br>';
-		message += "You've been invited to Phanime's closed beta."
-		message += "We've attached a unique code to this email that you can use to sign up."
-		message += "Code: " + code;
-		message += "Paste this code into the code field on the <a href='http://alpha.phanime.com/sign-up'>sign-un</a> page to create an account and get full access to Phanime!";
+		message += "<div>You've been invited to Phanime's closed beta.</div>"
+		message += "<div>We've attached a unique code to this email that you can use to sign up.</div>"
+		message += "<h3>Code: " + code + "</h3>";
+		message += "<div>Paste this code into the code field on the <a href='http://alpha.phanime.com/sign-up'>sign-up</a> page to create an account and get full access to Phanime's closed beta!</div>";
+		message += "<br><br>";
 		message += "<div>Sincerely,</div>";
 		message += "<div>The Phanime Team</div>";
 
