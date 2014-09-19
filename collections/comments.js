4e99d8745b832f5699a37e8844e1b23ac849a77c
MaazAli <maaz_ali@outlook.com>
2014-09-19 16:32:58 -0400

    Added a comments collection

---
+++ collections/comments.js
@@ -0,0 +1,21 @@
+Comments = new Meteor.Collection('comments');
+
+Comments.allow({
+
+	insert: function(userId, doc) {
+		// the user must be logged in, and the comment must be created by the user
+		return (userId && doc.posterId === userId);
+	},
+	update: function(userId, doc, fields, modifier) {
+
+		// can only update their own comments
+		return doc.userId === userId;
+
+	},
+	remove: function(userId, doc) {
+
+		// can only remove comments that they created
+		return doc.userId === userId;
+	}
+
+});
