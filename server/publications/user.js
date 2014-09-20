b66291becc58266822dcf7a50fbc7022b5269669
MaazAli <maaz_ali@outlook.com>
2014-09-20 10:41:33 -0400

    Added comments and it's poster into user publication

--- server/publications/user.js
+++ server/publications/user.js
@@ -19,9 +19,30 @@ Meteor.publishComposite('userWithProfilePosts', function(username) {
 						// Publish the poster as well if it wasn't a status update
 						find: function(profilePost, user) {
 							if (profilePost.statusUpdate === false) {
-								return Meteor.users.find({_id: profilePost.posterId});
+								return Meteor.users.find({_id: profilePost.posterId}, {fields: {username: 1}});
 							}
 						}
+					},
+					{
+
+						// Publish the profile post's comments
+						find: function(profilePost, user) {
+
+							return Comments.find({contentId: profilePost._id, type: 'profilePost'});
+
+						},
+						children : [
+							{
+								find: function(comment, profilePost, user) {
+									// Publish users if it isn't published 
+									if (comment.userId !== user._id && comment.userId !== profilePost.posterId) {
+										return Meteor.users.find({_id: comment.userId}, {fields: {username: 1}});
+									}
+								}
+							}
+
+						]
+
 					}
 
 				]
