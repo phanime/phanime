8f105d33d48c92487aa53f7c9d32e753dbf5d017
MaazAli <maaz_ali@outlook.com>
2014-09-20 10:10:10 -0400

    Comments are now created properly using the enter key

--- client/views/comments/createComment.js
+++ client/views/comments/createComment.js
@@ -3,8 +3,8 @@ Template.createComment.events({
 	'keypress .create-comment textarea' : function (event, template) {
 
 		// Only execute the below code if the enter key was pressed
-		if (enterkey was pressed only) {
-			var commentContent = $(event.target).val();
+		if (event.keyCode == 13 && !event.shiftKey) {
+			var commentContent = $(event.target).val().trim();
 			var profilePost = template.data;
 
 			// Quick check that it's not empty 
@@ -16,9 +16,11 @@ Template.createComment.events({
 					content: commentContent,
 					userId: Meteor.userId(),
 					createdAt: new Date(),
-				}
+				};
+
 
 				Comments.insert(comment, function(error, result) {
+					console.log(error);
 					if (!error) {
 						// Clear comment area
 						$(event.target).val('');
