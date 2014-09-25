ac886311ad69fd4a2c276c673bcd50596939b982
MaazAli <maaz_ali@outlook.com>
2014-09-25 00:00:18 -0400

    Library Entries now have a limited anime document sent with them

--- server/lib/requireCollectionFields.js
+++ server/lib/requireCollectionFields.js
@@ -2,7 +2,8 @@
 requireCollectionFields = {
 	
 	anime: {
-		imageAndTitle: {coverImage: 1, canonicalTitle: 1, englishTitle: 1, romajiTitle: 1}
+		imageAndTitle: {slug: 1, coverImage: 1, canonicalTitle: 1, englishTitle: 1, romajiTitle: 1},
+		requiredLibraryEntry: {slug: 1, coverImage: 1, canonicalTitle: 1, englishTitle: 1, romajiTitle: 1, totalEpisodes: 1}
 	},
 	person: {
 		imageAndName: {coverImage: 1, firstName: 1, lastName: 1}
