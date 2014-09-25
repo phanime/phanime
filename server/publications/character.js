ceaacfe53d3803bd20f0253dfa80553b9d899449
MaazAli <maaz_ali@outlook.com>
2014-09-24 23:47:41 -0400

    Updated the character publication to include related casting

--- server/publications/character.js
+++ server/publications/character.js
@@ -1,6 +1,30 @@
 // Publish a specific character by ID
 
-Meteor.publish('character', function(characterId) {
-	return Characters.find({_id: characterId});
-});
 
+Meteor.publishComposite("character", function(characterId) {
+
+	return {
+		find: function() {
+			return Characters.find({_id: characterId});
+		},
+		children: [
+			{
+				find: function(character) {
+					return Castings.find({characterId: character._id});
+				},
+				children: [
+					{
+						find: function(casting, character) {
+							return Anime.find({_id: casting.animeId});
+						}
+					},
+					{
+						find: function(casting, character) {
+							return People.find({_id: casting.personId});
+						}
+					}
+				]  
+			}
+		]
+	};
+});
