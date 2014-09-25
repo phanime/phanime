62bb72866c05ca9d2b10bfcde3bcd0fad16ac4c3
MaazAli <maaz_ali@outlook.com>
2014-09-24 23:56:12 -0400

    Defined helpers to ensure only fields that are needed are published

---
+++ server/lib/requireCollectionFields.js
@@ -0,0 +1,12 @@
+// Helpers to determine the fields we need when publishing
+requireCollectionFields = {
+	
+	anime: {
+		imageAndTitle: {coverImage: 1, canonicalTitle: 1, englishTitle: 1, romajiTitle: 1}
+	},
+	person: {
+		imageAndName: {coverImage: 1, firstName: 1, lastName: 1}
+	}
+
+
+}
