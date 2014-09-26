908a297d19bb4dde345c8e56c7ce52a6bfc4ab0c
MaazAli <maaz_ali@outlook.com>
2014-09-26 00:45:05 -0400

    Recommendations are now properly calculated

--- server/cron-jobs/generateAnimeRecommendations.js
+++ server/cron-jobs/generateAnimeRecommendations.js
@@ -2,7 +2,7 @@ SyncedCron.add({
 	name: 'Generate Anime Recommendations',
 	schedule: function(parser) {
 		// parser is a later.parse object
-		return parser.text('every 24 hours');
+		return parser.text('every 1 minutes');
 	}, 
 	job: function() {
 		Meteor.call('generateAnimeRecommendations', 'rFyM4MiRNRDyH35nZ', function(error, results) {
--- server/methods/recommendations.js
+++ server/methods/recommendations.js
@@ -3,12 +3,18 @@ Meteor.methods({
 	generateAnimeRecommendations: function(userId) {
 		// Let's grab user's library entries.
 		var libraryEntries = LibraryEntries.find({userId: userId}).fetch();
-		var recommendedAnime = [];
+		
+		// These are anime that we cannot recommend again! 
+		var animeIdsAlready = _.pluck(libraryEntries, 'animeId');
+		var recommendedAnime = {};
+
+
+
 		libraryEntries.forEach(function(libraryEntry) {
 			var anime = Anime.findOne({_id: libraryEntry.animeId});
 			// Ignore any entry that's rated below 7, or is dropped
-			if (libraryEntry.rating >= 7 && libraryEntry.status !== 'Dropped') {
-				var potentialAnime = Anime.find({genres: {$in: anime.genres}}).fetch();
+			if (libraryEntry.rating >= 7 || libraryEntry.status !== 'Dropped') {
+				var potentialAnime = Anime.find({genres: {$in: anime.genres}, _id: {$nin: animeIdsAlready}}).fetch();
 				
 				potentialAnime.forEach(function(anime) {
 					if (recommendedAnime[anime._id])
@@ -22,13 +28,28 @@ Meteor.methods({
 
 		});
 
-		console.log(recommendedAnime.sort(function(a,b) {
-			return b-a;
-		}));
 
-		// recommendedAnime.sort().reverse();
+		// We generate recommendation objects from these
+		recommendedAnime = _.map(recommendedAnime, function(num, key) {
+			// These will later on have multiple metrics
+			var obj = {
+				animeId: key,
+				score: num
+			};
+			return obj;
+		});
 
-		// console.log(recommendedAnime);
+
+		// sort them in desc order
+		recommendedAnime = _.sortBy(recommendedAnime, 'score').reverse();
+
+		// Get the top 10 recommendations
+		if (recommendedAnime.length > 10) {
+			recommendedAnime = recommendedAnime.slice(0, 10);
+		}
+
+		// Update the recommendations in the user document
+		Meteor.users.update({_id: userId}, {$set: {recommendedAnime: recommendedAnime}});
 
 	}
 
