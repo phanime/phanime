80c35a6a2f83ec3797d7a761e9dcc0b6d5ad89c6
MaazAli <maaz_ali@outlook.com>
2014-10-04 16:29:56 -0400

    Updated the revisionsQueue template and more

---
+++ client/views/moderator/revisionsQueue/revisionsQueueRow.js
@@ -0,0 +1,40 @@
+Template.revisionsQueueRow.created = function() {
+	this.isExpanded = new ReactiveVar(false);
+};
+
+
+Template.revisionsQueueRow.isExpanded = function() {
+	return Template.instance().isExpanded.get();
+};
+
+
+Template.revisionsQueueRow.events({
+
+	'click tr.queueRow' : function(event, template) {
+
+		template.isExpanded.set(!template.isExpanded.get());
+
+	},
+
+	'click .approveRevision' : function(event, template) {
+
+		console.log(template.data);
+
+		// Approving the revision basically means we'll be adding the updated data to our database
+
+		// We'll also update the revision's status to Approved here
+
+	},
+
+	'click .declineRevision' : function(event, template) {
+
+
+		console.log(template.data);
+
+		// We'll update the revision's status to Declined here
+
+
+
+	}
+
+});
