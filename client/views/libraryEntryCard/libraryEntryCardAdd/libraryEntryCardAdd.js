d4fda39467538a9551b6f232e6893b6bd907a2f1
MaazAli <maaz_ali@outlook.com>
2014-10-05 18:58:14 -0400

    Adding functionality added to animeCards

--- client/views/libraryEntryCard/libraryEntryCardAdd/libraryEntryCardAdd.js
+++ client/views/libraryEntryCard/libraryEntryCardAdd/libraryEntryCardAdd.js
@@ -13,45 +13,32 @@ Template.libraryEntryCardAdd.events({
 	// Change the status
 	'click .status-item' : function(event, template) {
 
+		var anime = template.data;
+
 		var status = $(event.target).text();
-		
 
-		console.log(status);
 
-		// // Set the current library entry
-		// var libraryEntry = template.data;
-		// var anime = libraryEntry.anime();
-		// if (libraryEntry) {
-		// 	// libraryEntry exists for the current user
+		// Check if it exists
+		var libraryEntry = LibraryEntries.findOne({userId: Meteor.userId(), animeId: anime._id});
 
-		// 	// If the user has selected remove as status
-		// 	// then we should delete their library entry
-		// 	if (status === 'Remove') {
-		// 		LibraryEntries.remove({_id: libraryEntry._id});
-		// 	}
 
-		// 	// But make sure the status is different
-		// 	if (status !== libraryEntry.status) {
+		// Ensure the library entry doesn't already exist
+		if (!libraryEntry) {
 
-		// 		LibraryEntries.update({_id: libraryEntry._id}, {$set: {
-		// 			status: status, 
-		// 			episodesSeen: (anime.totalEpisodes && status === 'Completed' ? anime.totalEpisodes : null),
-		// 			updatedAt: new Date(),
-		// 		}});
+			var currentEntry = {
+				userId: Meteor.userId(),
+				type: 'anime',
+				animeId: anime._id,
+				status: status,
+				episodesSeen: (anime.totalEpisodes && status === 'Completed' ? anime.totalEpisodes : null),
+				createdAt: new Date(),
+			};
 
-		// 		var libraryEntryActivity = Activity.libraryEntryFields('anime', anime._id, 'status', status);
-
-		// 		// Generate an activity for this action
-		// 		Meteor.call('createActivity', 'libraryEntry', Meteor.user()._id, libraryEntryActivity, function(error, result) {
-		// 			// console.log(error);
-		// 			// console.log(result);
-		// 		});
-
-		// 		//Notifications.success('Library Entry Updated', 'Your library entry status was successfully updated');
-
-		// 	} else {
-		// 		console.log('Statuses same, don\'t update');
-		// 	}
-		// }
+			LibraryEntries.insert(currentEntry, function(error, result) {
+				console.log(error);
+				console.log(result);
+			});
+			
+		}
 	}
 });
