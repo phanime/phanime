acf86eb28039bd6f23709e641d4aa53c745d8e66
MaazAli <maaz_ali@outlook.com>
2014-11-06 01:14:46 -0500

    New package added to import MAL user lists, this package will eventually be separated out of the core

---
+++ packages/phanime:mal-import/package.js
@@ -0,0 +1,30 @@
+Package.describe({
+	name: 'phanime:mal-import',
+	summary: 'Imports MAL user anime lists into phanime',
+	version: '1.0.0',
+	git: ''
+});
+
+Npm.depends({
+	'xml2js': '0.4.4'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0');
+	api.use([
+		'iron:router',
+		'reactive-var',
+		'spacebars',
+		'templating',
+		'ui',
+		'underscore',
+		'http'
+	], ['client', 'server']);
+
+	// Additions to the server
+	api.addFiles([
+		'server/methods/getUserList.js'
+	], ['server']);
+
+
+});
