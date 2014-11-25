93edccb70e2ce8bbf36cb8dfdd4dcd6058c90f16
MaazAli <maaz_ali@outlook.com>
2014-11-24 22:44:00 -0500

    model tests added (only for anime and libraryEntries) for now

---
+++ tests/mocha/server/models/anime.js
@@ -0,0 +1,42 @@
+if (!(typeof MochaWeb === 'undefined')){
+	MochaWeb.testOnly(function(){
+		var expect = chai.expect;
+		var should = chai.should;
+		describe("Anime", function(){
+			describe("create", function(){
+				var anime = {
+					canonicalTitle: "Test Anime Title",
+					type: "TV",
+					language: ["Subbed"],
+					ageRating: "R - 17+ (violence & profanity)",
+					genres: ["Adventure"]
+				};
+
+				before(function() {
+					Anime.insert(anime);
+				});
+				
+				it("should create anime 'Test Anime Title`", function() {
+
+					var anime2 = Anime.findOne(anime);
+
+					expect(anime2).to.be.an('object');
+
+					for(var key in anime) {
+
+						expect(anime2).to.have.property(key);
+						expect(anime2[key]).to.deep.equal(anime[key]);
+
+					}
+
+					chai.expect("1").to.be.a('string');
+
+				});
+
+				after(function() {
+					Anime.remove({});
+				});
+			});
+		});
+	});
+}
---
+++ tests/mocha/server/models/libraryEntry.js
@@ -0,0 +1,103 @@
+if (!(typeof MochaWeb === 'undefined')){
+	MochaWeb.testOnly(function(){
+		var expect = chai.expect;
+		var should = chai.should;
+		describe("LibraryEntry", function(){
+			before(function() {
+				// setup environment by creating a user
+
+				RequestedInvites.insert({used: false});
+
+				var user = {
+					username: "maaz",
+					email: "maaz@gmail.com",
+					password: "sup",
+					profile: {
+						signUpCode: RequestedInvites.findOne({used: false})._id
+					}
+				};
+
+				Accounts.createUser(user);
+
+				// Create an anime
+				var anime = {
+					canonicalTitle: "Test Anime Title",
+					type: "TV",
+					language: ["Subbed"],
+					ageRating: "R - 17+ (violence & profanity)",
+					genres: ["Adventure"]
+				};
+
+				Anime.insert(anime);
+			});
+			describe("create", function(){
+				before(function() {
+					// Let's grab the user we created
+					var userId = Meteor.users.findOne()._id;
+					var animeId = Anime.findOne()._id;
+					var libraryEntry = {
+						userId: userId,
+						animeId: animeId,
+						type: 'anime',
+						status: 'Watching',
+					};
+					LibraryEntries.insert(libraryEntry);
+				});
+				it("should have a createdAt field", function() {
+					var entry = LibraryEntries.findOne();
+					expect(entry).to.be.an('object');
+					expect(entry).to.have.property('createdAt').to.be.a('Date');
+				});
+
+				it("should not have an updatedAt field", function() {
+					var entry = LibraryEntries.findOne();
+					expect(entry).to.be.an('object');
+					expect(entry).to.not.have.property('updatedAt');					
+				});
+
+				after(function() {
+					// remove all libraryEntries created
+					LibraryEntries.remove({});
+				});
+			});
+			describe("update", function(){
+				before(function() {
+					// Let's grab the user we created
+					console.log(Meteor.users.find().fetch());
+					console.log(Anime.find().fetch());
+					var userId = Meteor.users.findOne()._id;
+					var animeId = Anime.findOne()._id;
+					var libraryEntry = {
+						userId: userId,
+						animeId: animeId,
+						type: 'anime',
+						status: 'Watching',
+					};
+					LibraryEntries.insert(libraryEntry);
+				});
+				it("should have an updatedAt field", function() {
+					var entry = LibraryEntries.findOne();
+					entry.status = 'Plan to watch';
+					LibraryEntries.update({_id: entry._id}, {$set: entry});
+					var entry = LibraryEntries.findOne();
+
+					expect(entry).to.be.an('object');
+					expect(entry).to.have.property('createdAt').to.be.a('Date');
+					expect(entry).to.have.property('updatedAt').to.be.a('Date');
+					expect(entry.createdAt.getTime()).to.be.below(entry.updatedAt.getTime());
+				});
+
+				after(function() {
+					// remove all libraryEntries created
+					LibraryEntries.remove({});
+				});
+			});
+			after(function() {
+				// clean up all the documents we created
+				Meteor.users.remove({});
+				Anime.remove({});
+				RequestedInvites.remove({});
+			});
+		});
+	});
+}
