fbe331f243f83dc62747d70ba3c3984571d02764
MaazAli <maaz_ali@outlook.com>
2014-12-02 16:31:22 -0500

    Simple animation introduced when creating a profile post and email template over-hauled

--- server/emails/resetPasswordTemplate.js
+++ server/emails/resetPasswordTemplate.js
@@ -4,18 +4,20 @@ Accounts.emailTemplates.resetPassword.subject = function (user) {
     return "How to reset your password on phanime";
 };
 Accounts.emailTemplates.resetPassword.html = function (user, url) {
-	var message = '<div style="font: Myriad Pro, sans-serif;">'; 
-	message += '<h1>phanime</h1><br>';
-	message += "<div>Hello " + user.originalUsername + ",</div><br>"
-	message += "<div>To reset your password, simply click the link below.</div>"
-	message += "<h3><a href=" + url + ">reset password</a></h3>";
-	message += "<div>If you've received this email in error or didn't request a password reset, you can simply ignore it.</div>";
-	message += "<br><br>";
-	message += "<div>Sincerely,</div>";
-	message += "<div>The Phanime Team</div>";
-	message += "</div>";
+	// var message = '<div style="font: Myriad Pro, sans-serif;">'; 
+	// message += '<h1>phanime</h1><br>';
+	// message += "<div>Hello " + user.originalUsername + ",</div><br>"
+	// message += "<div>To reset your password, simply click the link below.</div>"
+	// message += "<h3><a href=" + url + ">reset password</a></h3>";
+	// message += "<div>If you've received this email in error or didn't request a password reset, you can simply ignore it.</div>";
+	// message += "<br><br>";
+	// message += "<div>Sincerely,</div>";
+	// message += "<div>The Phanime Team</div>";
+	// message += "</div>";
 
-	return message;
+	return Spacebars.toHTML({ url: url }, Assets.getText('resetPassword.html'));
+	// return Assets.getText('resetPassword.html');
+	// return message;
 };
 
 Accounts.emailTemplates.resetPassword.text = function (user, url) {
