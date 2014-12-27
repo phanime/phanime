e967683f354574756925aebf0fcb0eed8d6d15c1
MaazAli <maaz_ali@outlook.com>
2014-12-27 03:26:35 -0500

    Profile fields are now properly saved

--- client/views/user/userSummary.js
+++ client/views/user/userSummary.js
@@ -5,11 +5,21 @@ Template.userSummary.created = function() {
 Template.userSummary.events({
 	'click #editProfile' : function(event, template) {
 		template.isEditing.set(!template.isEditing.get());
-		console.log('test');
 	},
 	'click #saveProfile' : function(event, template) {
 		// Let's save things 
+		var about = $("#profileAboutInput").val().trim();
+		var location = $("#profileLocationInput").val().trim();
+		var website = $("#profileWebsiteInput").val().trim();
 
+		var currentProfile = template.data.profile;
+		var newProfile = currentProfile;
+		
+		Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.about': about, 'profile.location': location, 'profile.website': website}});
+		Notifications.success("Profile changes saved!", "Your profile has been successfully saved");
+
+		// Toggle the isEditing reactive var
+		template.isEditing.set(!template.isEditing.get());
 	}
 });
 
