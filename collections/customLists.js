293719fae1fd092fa0d1c09d7e82466a64359cc6
MaazAli <maaz_ali@outlook.com>
2014-12-29 06:18:34 -0500

    Custom list schema / collection added

---
+++ collections/customLists.js
@@ -0,0 +1,91 @@
+CustomLists = new Meteor.Collection("customLists");
+
+CustomListsSchema = new SimpleSchema({
+	type: {
+		type: String,
+		allowedValues: ['anime', 'manga', 'character', 'people']
+	},
+	title: {
+		type: String,
+		min: 1,
+		max: 100 // Not sure what we should set as a hard limit
+	},
+	slug: {
+		type: String,
+		autoValue: function() {
+			var title;
+			if (this.isUpdate) {
+				title = CustomLists.findOne({_id: this.docId}).title;
+			} else {
+				title = this.field("title").value;
+			}
+
+			return getSlug(title);
+		}
+	},
+	description: {
+		type: String,
+		min: 1,
+		max: 500,
+		optional: true
+	},
+	entries: {
+		type: [Object],
+		minCount: 1,
+		maxCount: 100 // We may need more then this... Any limit that we have should be a strict one (We want to keep the documents small in size)
+	},
+	"entries.$.contentId": {
+		type: String
+		// Can't really do a check at the moment
+	},
+	"entries.$.sortOrder": {
+		type: Number,
+		min: 1,
+		max: 100, // This max should match with the entries maxCount
+	},
+	"entries.$.comment": {
+		type: Number,
+		min: 1,
+		max: 500,
+		optional: true
+	},
+	privacy: {
+		type: Boolean,
+		optional: true
+	},
+	disableComments: {
+		type: Boolean,
+		optional: true
+	},
+	createdAt: {
+		type: Date,
+		autoValue: function() {
+			if (this.isInsert) {
+				return new Date();
+			} else if (this.isUpsert) {
+				return {$setOnInsert: new Date()};
+			} else {
+				this.unset();
+			}
+			//  else if ((this.value === null || this.value === undefined) && !this.isUpdate) {
+			// 	// this is more for the times where validation needs to happen without "inserting"
+			// 	return new Date();
+			// }
+		},
+		denyUpdate: true,
+		optional: true // this is only made optional because validation before insert will not work if it was required, however, this does not make much of a difference as the createdAt value will still be generated on insert.
+	},
+	updatedAt: {
+		type: Date,
+		autoValue: function() {
+			if (this.isUpdate) {
+				return new Date();
+			}
+		},
+		denyInsert: true,
+		optional: true
+	}
+});
+
+
+CustomLists.attachSchema(CustomListsSchema);
