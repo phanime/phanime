11d5fe9425d5070e28eab62f96329f72db3bbe0e
MaazAli <maaz_ali@outlook.com>
2015-01-18 20:44:21 -0500

    polymer elements removed from the signIn/signUp pages, and an observer added

--- server/publications/moderatorOpenRevisionCount.js
+++ server/publications/moderatorOpenRevisionCount.js
@@ -2,7 +2,7 @@ Meteor.publish('moderatorOpenRevisionCount', function() {
 	// Mainly used for counts
 	// only publish this if the user is actually a moderator
 	var user = Meteor.users.findOne({_id: this.userId});
-	if (user.isModerator()) {
+	if (user && user.isModerator()) {
 		Counts.publish(this, 'openRevisions', Revisions.find({status: 'Open'}));
 	}
 });
