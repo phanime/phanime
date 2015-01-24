efc6bf742f376a817376ff55681fec248ecc62a0
MaazAli <maaz_ali@outlook.com>
2015-01-23 22:24:48 -0500

    Added updatedAt and createdAt fields

--- collections/episodes.js
+++ collections/episodes.js
@@ -39,9 +39,31 @@ EpisodesSchema = new SimpleSchema({
 	description: {
 		type: String,
 		optional: true
+	},
+	createdAt: {
+		type: Date,
+		autoValue: function() {
+			if (this.isInsert) {
+				return new Date();
+			} else if (this.isUpsert) {
+				return {$setOnInsert: new Date()};
+			} else {
+				this.unset();
+			}
+		},
+		denyUpdate: true,
+		optional: true // this is only made optional because validation before insert will not work if it was required, however, this does not make much of a difference as the createdAt value will still be generated on insert.
+	},
+	updatedAt: {
+		type: Date,
+		autoValue: function() {
+			if (this.isUpdate) {
+				return new Date();
+			}
+		},
+		denyInsert: true,
+		optional: true // this is only made optional because validation before insert will not work if it was required, however, this does not make much of a difference as the value will still be generated on update.
 	}
-
-
 });
 
 Episodes.attachSchema(EpisodesSchema);
