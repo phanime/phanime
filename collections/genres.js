96a782ad89de449d8705beb80af56a9b07b1e6f1
MaazAli <maaz_ali@outlook.com>
2015-01-23 21:59:16 -0500

    Added a schema for genres

--- collections/genres.js
+++ collections/genres.js
@@ -1,1 +1,29 @@
 Genres = new Meteor.Collection("genres");
+
+GenresSchema = new SimpleSchema({
+	name: {
+		type: String
+	},
+	slug: {
+		type: String,
+		autoValue: function() {
+			var name;
+
+			if (this.isUpdate && !this.field("name").value) {
+				name = Genres.findOne({_id: this.docId}).name;
+			} else {
+				name = this.field("name").value;
+			}
+
+			console.log(name);
+
+			return getSlug(name);
+		}
+	},
+	description: {
+		type: String,
+		optional: true
+	}
+});
+
+Genres.attachSchema(GenresSchema);
