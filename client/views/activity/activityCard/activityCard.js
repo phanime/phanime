27ed15f4c08446a5a30963fcf85fd0f565c43809
MaazAli <maaz_ali@outlook.com>
2015-01-27 01:44:13 -0500

    Activities can now be removed

--- client/views/activity/activityCard/activityCard.js
+++ client/views/activity/activityCard/activityCard.js
@@ -3,3 +3,12 @@ Template.activityCard.helpers({
 		return this.type === type;
 	}
 });
+
+Template.activityCard.events({
+
+	'click .remove-handle' : function(event, template) {
+		var activity = template.data;
+		Activity.remove({_id: activity._id});
+	}
+
+});
