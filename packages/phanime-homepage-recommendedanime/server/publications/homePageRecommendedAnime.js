ddef1670341d2e3cfd61c5bdff09d50b848cc4e7
MaazAli <maaz_ali@outlook.com>
2015-02-02 00:39:51 -0500

    phanime homepage recommended anime module fixed

--- packages/phanime-homepage-recommendedanime/server/publications/homePageRecommendedAnime.js
+++ packages/phanime-homepage-recommendedanime/server/publications/homePageRecommendedAnime.js
@@ -1,4 +1,4 @@
-Meteor.publishComposite('homePageActiveEntries', function() {
+Meteor.publishComposite('homePageRecommendedAnime', function() {
 	return {
 		find: function() {
 			return Meteor.users.find({_id: this.userId});
@@ -9,7 +9,14 @@ Meteor.publishComposite('homePageActiveEntries', function() {
 					var animeIds = _.pluck(user.recommendedAnime, 'animeId');
 					// Limit to 12 recommendations for now
 					return Anime.find({_id: {$in: animeIds}}, {fields: requireCollectionFields.anime.requiredLibraryEntry, limit: 12});
-				}
+				},
+				children: [
+					{
+						find: function(anime, user) {
+							return LibraryEntries.find({userId: user._id, animeId: anime._id});
+						}
+					}
+				]
 			}
 		]
 	};
