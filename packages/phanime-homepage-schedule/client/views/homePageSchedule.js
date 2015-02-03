e4e481765e34e9b3021e3709fb8c89c48d35ce04
MaazAli <maaz_ali@outlook.com>
2015-02-03 00:26:01 -0500

    Setting up the schedule structure for the home page schedule module

---
+++ packages/phanime-homepage-schedule/client/views/homePageSchedule.js
@@ -0,0 +1,67 @@
+Template.homePageSchedule.created = function() {
+	var self = this;
+	self.ready = new ReactiveVar(false);
+	self.schedule = new ReactiveVar();
+
+	// Temp
+	var days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+	];
+
+	var subscription = Meteor.subscribe("homePageSchedule");
+
+	self.autorun(function() {
+		if (subscription.ready()) {
+
+			var anime = Anime.find({
+						$and: [
+							{
+								$or: [{
+									endDate: {$gte: new Date()}
+								},
+								{
+									endDate: {$exists: false}
+								},
+								{
+									endDate: "0000-00-00" // For the dates that are still represented as strings
+								}]
+							},
+							{
+								startDate: {
+									$lt: new Date()
+								}
+							}
+						]
+					});
+
+
+			var animeIds = _.pluck(anime.fetch(), '_id');
+
+			var libraryEntries = LibraryEntries.find({status: "Watching", animeId: {$in: animeIds}});
+			var schedule = [];
+
+			libraryEntries.forEach(function(entry) {
+				var anime = Anime.findOne({_id: entry.animeId});
+				var dayNumber = moment(anime.startDate).day();
+				if (schedule[dayNumber]) {
+					schedule[dayNumber].push(entry);
+				} else {
+					schedule[dayNumber] = [entry];
+				}
+			});
+
+
+			debugger;
+
+			self.ready.set(true);
+		} else {
+			self.ready.set(false);
+		}
+	});
+};
