0438aab365fa14cb436bc6ba523701f28405906b
MaazAli <maaz_ali@outlook.com>
2015-02-07 21:36:39 -0500

    Added the ability to mention users in profile posts

--- client/views/profilePost/createProfilePost/createProfilePost.js
+++ client/views/profilePost/createProfilePost/createProfilePost.js
@@ -28,20 +28,33 @@ Template.createProfilePost.events({
 				content: content
 			};
 
-			ProfilePosts.insert(profilePost, function(error, result) {
+			ProfilePosts.insert(profilePost, function(error, _id) {
 				if (!error) {
 					// Clear the activity create textarea if everything went well
 					$('#createProfilePostContent').val('');
 
-
+					console.log(_id);
 					// Add the poster's name in the profile post
 					profilePost.posterUsername = Meteor.user().displayName();
-					// Send out an alert as long as it wasn't a status update					
+					profilePost.profilePostId = _id;
+					// Send out an alert as long as the poster isn't the one thats getting alerted					
 					if (statusUpdate === false) {
 						Meteor.call('createAlert', 'userProfilePost', profilePost, user._id, function(error, result) {
 							// Nothing of interest to put here really.
 						});
 					}
+
+
+					Meteor.call('phanimeLib__parseTextForMentions', content, function(error, result) {
+						if (error) {
+							Notifications.error('Failed to create a profile post', error.reason);
+						} else {
+							Meteor.call('phanimeAlerts__alertUsernames', 'mentionProfilePost', profilePost, result, function(error, result) {
+								console.log(error);
+								console.log(result);
+							});
+						}
+					});
 				} else {
 					Notifications.error('Profile post creation failed', error.reason);
 				}
