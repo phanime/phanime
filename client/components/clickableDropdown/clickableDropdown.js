6bc5a21ea6ae3837c158599c7e30e762d04fa598
MaazAli <maaz_ali@outlook.com>
2015-03-15 19:48:31 -0400

    Clickable dropdown component now properly closes

--- client/components/clickableDropdown/clickableDropdown.js
+++ client/components/clickableDropdown/clickableDropdown.js
@@ -1,11 +1,29 @@
+Template.clickableDropdown.onCreated(function() {
+	this.isOpen = new ReactiveVar(false);
+});
+
 Template.clickableDropdown.events({
-  'click .clickable-dropdown__handler' : function(event, template) {
-    var dropdownEl = $(event.target).parents('.clickable-dropdown').children('.clickable-dropdown__menu');
-    var isOpen = dropdownEl.hasClass('clickable-dropdown__menu--open');
-    if (isOpen) {
-      dropdownEl.removeClass('clickable-dropdown__menu--open');
-    } else {
-      dropdownEl.addClass('clickable-dropdown__menu--open');
-    }
-  }
+	'click .clickable-dropdown__handler' : function(event, template) {
+		var dropdownEl = $(event.target).parents('.clickable-dropdown').children('.clickable-dropdown__menu');
+		var isOpen = dropdownEl.hasClass('clickable-dropdown__menu--open');
+		if (isOpen) {
+			template.isOpen.set(false);
+			dropdownEl.removeClass('clickable-dropdown__menu--open');
+		} else {
+			template.isOpen.set(true);
+			dropdownEl.addClass('clickable-dropdown__menu--open');
+		}
+	},
+
+	'click .clickable-dropdown__backdrop' : function(event, template) {
+		var dropdownEl = $(event.target).parents('.clickable-dropdown').children('.clickable-dropdown__menu');
+		dropdownEl.removeClass('clickable-dropdown__menu--open');
+		template.isOpen.set(false);
+	}
+});
+
+Template.clickableDropdown.helpers({
+	isOpen: function() {
+		return Template.instance().isOpen.get();
+	}
 });
