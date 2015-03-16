b4d0d30a1bd09a177b628ed7e973f01f8b522a79
MaazAli <maaz_ali@outlook.com>
2015-03-16 00:59:26 -0400

    Re-structure .styl files to component level and update profile-posts

--- client/components/profilePost/profilePostCard/profilePostCard.js
+++ client/components/profilePost/profilePostCard/profilePostCard.js
@@ -36,6 +36,44 @@ Template.profilePostCard.events({
 
 Template.profilePostCard.helpers({
 
+	formatContent: function(text) {
+
+		// We first want to escape all the html if there is any
+		var text = _.escape(text);
+		var returnedHtml = "";
+		var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+		var userRegex = /@[a-zA-Z0-9_]+/g;
+
+		// Only one image allowed per post, the rest will be turned into urls
+		var imageTrigger = false;
+		var ourImage;
+
+		text = text.replace(urlRegex, function(url,b,c) {
+			var url2 = (c == 'www.') ?  'http://' +url : url;
+			var imageRegex = /^http:\/\/.+\.(gif|png|jpg|jpeg)$/i;
+			if (imageRegex.test(url2) && imageTrigger === false) {
+				imageTrigger = true;
+				ourImage = '<div><a href="' + url2 + '" target="_blank"><img src="' + url2 + '"></a></div>'; 
+				return '';
+			} else {
+				return '<a href="' +url2+ '" target="_blank">' + url + '</a>';
+			}
+		}); 
+
+		text = text.replace(userRegex, function(match) {
+			var userProfileUrl = Router.routes['user'].path({username: match.slice(1, match.length).toLowerCase()});
+			return '<a href="' + userProfileUrl + '" class="user-mention">' + match + "</a>";
+		});
+
+		if (ourImage) {
+			returnedHtml = '<div class="profile-post-card__featured">' + ourImage + "</div>";
+		}
+
+		returnedHtml += '<div class="description profile-post-card__text">' + text + '</div>';
+
+		return returnedHtml;
+	},
+
 	likeText: function() {
 		var profilePost = Template.instance().data;
 		var likes = profilePost.likes;
