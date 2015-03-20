833e5e306ce5f0068da1d6e46f238127c464b31d
MaazAli <maaz_ali@outlook.com>
2015-03-19 21:45:22 -0400

    Limit search results to 20 for now

--- server/publications/customList/customListContentSearch.js
+++ server/publications/customList/customListContentSearch.js
@@ -5,16 +5,11 @@ Meteor.publishComposite("customListContentSearch", function(query, type) {
 			
 			switch (type) {
 				case "anime":
-					return Anime.find({canonicalTitle: new RegExp(query)});
-					break;
+					return Anime.find({canonicalTitle: new RegExp(query)}, {limit: 20});
 				case "characters":
-					return Characters.find({$text: {$search: query}});
-					break;
+					return Characters.find({$text: {$search: query}}, {limit: 20});
 				case "people":
-					return People.find({$text: {$search: query}});
-					break;
-				default: 
-					break;
+					return People.find({$text: {$search: query}}, {limit: 20});
 			}
 
 		}
--- server/publications/user/userLibrary/userLibrarySection/userLibrarySearch.js
+++ server/publications/user/userLibrary/userLibrarySection/userLibrarySearch.js
@@ -4,9 +4,9 @@ Meteor.publishComposite('userLibrarySearch', function(user, query) {
 			// If user's profile is not current profile we don't publish private entries
 			var libraryEntries;
 			if (user._id !== this.userId) {
-				var libraryEntries = LibraryEntries.find({userId: user._id, canonicalTitle: new RegExp(query), privacy: {$ne: true}});
+				var libraryEntries = LibraryEntries.find({userId: user._id, canonicalTitle: new RegExp(query), privacy: {$ne: true}}, {limit: 20});
 			} else {
-				var libraryEntries = LibraryEntries.find({userId: user._id, canonicalTitle: new RegExp(query)});
+				var libraryEntries = LibraryEntries.find({userId: user._id, canonicalTitle: new RegExp(query)}, {limit: 20});
 			}
 			return libraryEntries;
 		},
